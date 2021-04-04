e7510d63d3d7fc37f873327f9df725ae4aff5302
phreakester <getty58@me.com>
2021-04-03 23:56:53 -0700

    We lost the final four :pensive:

--- src/components/Mastiff.js
+++ src/components/Mastiff.js
@@ -6,7 +6,7 @@ function Mastiff() {
             <div className="logo-text">
                 <a href="https://google.com">Cafe du Tom</a>
             </div>
-            <div className="main-body">
+            <div className="intro-text">
             Hello, and thank you for using the Cafe du Tom virtual ordering service! Simply enter your order from the menu below, enter your phone number, and we will text you when your order is available.
             </div>
         </div>
