61f7bc32780d22da621a84e10a86414b0d8308be
phreakester <gettyv05@gmail.com>
2021-04-06 12:01:57 -0700

    Added data flow for modifications selector so that it will update as the user sets their preferences

--- src/components/LevelSelector.js
+++ src/components/LevelSelector.js
@@ -10,11 +10,17 @@ function LevelButton(props) {
 
 function LevelSelector(props) {
     const [level, setLevel] = useState(1); //Level can be 0,1,2
+
+    function changeLevel (new_value) {
+        setLevel(new_value);
+        props.setModifications(props.added, new_value);
+    }
+
     return(
         <div className="level-selector">
-            <LevelButton level={level} value={0} text={"Low " + props.added} setLevel={setLevel} />
-            <LevelButton level={level} value={1} text={"Normal " + props.added} setLevel={setLevel} />
-            <LevelButton level={level} value={2} text={"Extra " + props.added} setLevel={setLevel}/>
+            <LevelButton level={level} value={0} text={"Low " + props.added} setLevel={changeLevel} />
+            <LevelButton level={level} value={1} text={"Normal " + props.added} setLevel={changeLevel} />
+            <LevelButton level={level} value={2} text={"Extra " + props.added} setLevel={changeLevel}/>
         </div>
     );
 }
--- src/components/MenuItem.js
+++ src/components/MenuItem.js
@@ -5,16 +5,30 @@ import LevelSelector from './LevelSelector'
 
 function MenuItem(props) {
   const item = props.item;
-  var itemToAdd = item;
+  var itemToAdd = item[0];
   const [isClicked, setIsClicked] = useState(false);
-  const [modificatons, setModifications] = useState(null);
+  const [modificatons, setModifications] = useState({});
+
+  function changeModifications (added, newValue) {
+    setModifications(modificatons[added] = newValue); //Modifications is an obj with added:level EX sugar:0
+    console.log(added + " : " + newValue )
+    console.log(JSON.stringify(modificatons))
+  }
+
+  function sendOrder () {
+    console.log(JSON.stringify(modificatons))
+    props.addToOrder(itemToAdd + modificatons);
+    console.log("Sent order to Menu: " + itemToAdd + modificatons);
+    console.log("itemtoadd: " + itemToAdd)
+    console.log(JSON.stringify(modificatons))
+  }
 
   return (
     isClicked ? ( //Change to the clicked version of the card, where they can specify sugar, milk, etc.
       <div className="item-card">
-        <LevelSelector added="sugar"/>
-        <LevelSelector added="creme" />
-        <button onClick = {() => {setIsClicked(false); props.addToOrder(itemToAdd);}}>Add to order?</button>
+        <LevelSelector added="sugar" setModifications={changeModifications}/>
+        <LevelSelector added="creme" setModifications={changeModifications}/>
+        <button onClick = {() => {setIsClicked(false); sendOrder();}}>Add to order?</button>
         <button onClick={() => setIsClicked(false)} >Cancel</button>
       </div>
     ) : (
